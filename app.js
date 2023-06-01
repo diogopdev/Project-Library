938de3236a3575057aaf2867148de4af679b6b21
Diogo Pereira <90282042+diogopdev@users.noreply.github.com>
2023-06-01 18:03:50 +0100

    Gets and Adds Book
    Created function to get book from input and adds it to a variable (newBook)

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
-// let myLibrary = [];
-
-// function Book(title, author, pages, read) {
-// 	this.title = title
-// 	this. author = author
-// 	this.pages = pages
-// 	this.read = read
-// }
-
-// function addBookToLibrary(){
-
-// }
-
+// Get Elements
 const addBookBtn = document.getElementById('addBookBtn')
 const addBookModal = document.getElementById('addBookModal')
 const addBookForm = document.getElementById('addBookForm')
 const btnCancelBook = document.getElementById('btnCancelBook');
 
+// Functions to create and close Modal
 const openAddBookModal = () => {
 	addBookForm.reset()
 	addBookModal.classList.add('active')
@@ -26,5 +15,37 @@ const closeAddBookModal = () => {
 	addBookModal.classList.remove('active')
 }
 
-addBookBtn.onclick = openAddBookModal
-btnCancelBook.onclick = closeAddBookModal
+// Classes for Books
+class Book {
+	constructor(
+		title = 'Unknown',
+		author = 'Unkown',
+		pages = '0',
+		isRead = false
+	){
+		this.title = title
+		this. author = author
+		this.pages = pages
+		this.isRead = isRead
+	}
+}
+
+const getBook = () => {
+	const title = document.getElementById('title').value
+	const author = document.getElementById('author').value
+	const pages = document.getElementById('pages').value
+	const isRead = document.getElementById('isRead').checked
+	return new Book(title, author, pages, isRead)
+}
+
+const addBook = (e) => {
+	e.preventDefault()
+	const newBook = getBook()
+	console.log(newBook)
+	closeAddBookModal()
+}
+
+// Button Clicks and function calls
+addBookBtn.onclick = openAddBookModal //Opens Modal
+btnCancelBook.onclick = closeAddBookModal //Closes Modal
+addBookForm.onsubmit = addBook //Submits Form
